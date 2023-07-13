98447926fb85603bb413fa2e2388875736ba557e
azeddine chakir <azeddine.chakir@um5r.ac.ma>
2023-07-13 07:47:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /*
  * main - Entry point
- *
  * Description: a simple function that uses main as entry point
  * then prints a simple statemnt with return code 0
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
