106ee61b774278852e2835c5125268917b7936bd
azeddine chakir <azeddine.chakir@um5r.ac.ma>
2023-07-13 07:41:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * a simple function that uses main as entry point then prints a simple statemnt with return code 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+    return (0);
+}
