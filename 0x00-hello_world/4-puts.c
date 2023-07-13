2c1751ab7a94fdc3666995e715d7c044c2887a8c
azeddine chakir <azeddine.chakir@um5r.ac.ma>
2023-07-13 07:19:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-/*
+/**
+ * main - Entry point
  *
- * The main function of this program declares a string, stored in the character pointer variable 'text'.
- * This string contains the statement: "Programming is like building a multilingual puzzle".
- * The function then prints this string to the standard output (typically the terminal or console)
- * using the 'puts' function from the standard I/O library (stdio.h).
- * The program returns 0 upon successful execution, signaling successful termination to the operating system.
+ * Description: Prints a string using puts function.
  *
+ * Return: Always 0 (Success)
  */
-
 
 int main(void)
 {
 	char *text = "\"Programming is like building a multilingual puzzle";
 
 	puts(text);
-	return 0;
+	return (0);
 }
