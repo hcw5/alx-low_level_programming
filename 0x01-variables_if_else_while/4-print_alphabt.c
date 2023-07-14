368922c6ce124f96a85c023cafa49e89486ae3c5
azeddine chakir <azeddine.chakir@um5r.ac.ma>
2023-07-14 09:10:16 -0700

    4-print_alphabets.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the alphabet in lowercase, excluding q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
