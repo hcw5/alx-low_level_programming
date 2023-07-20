55737e34318de4762abffb64b85dd002b105661b
azeddine chakir <azeddine.chakir@um5r.ac.ma>
2023-07-20 10:31:54 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isdigit - checks for digits 0 to 9
+* @c: character to be checked.
+* Return: 1 if true and 0 if false
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
