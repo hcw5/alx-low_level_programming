d39b1f39d65da835b7df403d38d6f6976257880d
azeddine chakir <azeddine.chakir@um5r.ac.ma>
2023-07-24 15:39:00 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* swap_int - Swaps values of variables
+* @a: First integer
+* @b: Second integer
+*/
+void swap_int(int *a, int *b)
+{
+	int p = *a;
+
+	*a = *b;
+	*b = p;
+}
