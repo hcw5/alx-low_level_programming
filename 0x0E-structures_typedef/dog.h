4ee45d4bc43e32897d3401a63d2e86c1c9b3d64a
Azeddine chakir <azeddine.chakir@um5r.ac.ma>
2023-11-06 22:12:39 +0100

    created README.md

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,18 @@
-#ifndef STRUCTS
-#define STRUCTS
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - description for a pet
- * @name: pet name
- * @age: pet age
- * @owner: owner
- *
- * Description: Longer description
- */
-typedef struct dog
+ * struct dog - user defined data type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-#endif
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
-int _putchar(char c);
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
