5a54f48d660a0145d2e4ff364eef0791baedd9b8
Azeddine chakir <azeddine.chakir@um5r.ac.ma>
2023-11-08 23:21:30 +0100

    forgot header file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include "function_pointers.h"
 /**
- * print_name - print the name
- * @name: name to print
- * @f: function to use
- */
+ * print_name - function thats prints a name passed to it
+ * @name: char to display to the stdout
+ * @f: A pointer function
+ * Return: No return cause we've declared VOID as R_TYPE
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name && f)
+		f(name);;
 }
